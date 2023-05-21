2efce34966debe59db3499d5924ebf72785e2398
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-05-21 05:52:10 -0400

    put gamblers ruin example into a function

--- LJSA_Reference_Examples.m
+++ LJSA_Reference_Examples.m
@@ -1,11 +1,13 @@
 clear all;
-N=1000;q=0.51; p=1-q;
-L=sparse(2:N-1,1:N-2,p*ones(1,N-2),N-1,N-1)
-U=sparse(1:N-2,2:N-1,q*ones(1,N-2),N-1,N-1)
-ImT=eye(N-1)-L-U;
-d=ones(1,N-1);
-tau=d/ImT;
-plot([1:N-1],tau,'k-','LineWidth',2);
-xlabel('Initial capital');
-ylabel('Expected duration');
-max(tau);
+function m = gamblersRuin(q, N) 
+    p=1-q;
+    L=sparse(2:N-1,1:N-2,p*ones(1,N-2),N-1,N-1)
+    U=sparse(1:N-2,2:N-1,q*ones(1,N-2),N-1,N-1)
+    ImT=eye(N-1)-L-U;
+    d=ones(1,N-1);
+    tau=d/ImT;
+    plot([1:N-1],tau,'k-','LineWidth',2);
+    xlabel('Initial capital');
+    ylabel('Expected duration');
+    m = max(tau);
+end
