7696fb894e795422078dd34d1ebe6c94e9875ab9
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-05-26 00:29:26 -0400

    Update LJSA_Reference_Examples.m

--- LJSA_Reference_Examples.m
+++ LJSA_Reference_Examples.m
@@ -1,4 +1,4 @@
-function m = gamblersRuin(q, N) 
+function = gamblersRuin(q, N) 
     p=1-q;
     L=sparse(2:N-1,1:N-2,p*ones(1,N-2),N-1,N-1)
     U=sparse(1:N-2,2:N-1,q*ones(1,N-2),N-1,N-1)
@@ -8,5 +8,4 @@ function m = gamblersRuin(q, N)
     plot([1:N-1],tau,'k-','LineWidth',2);
     xlabel('Initial capital');
     ylabel('Expected duration');
-    m = max(tau);
 end
