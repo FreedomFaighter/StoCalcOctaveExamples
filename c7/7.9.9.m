626bad51baaedc8fd6a93153e2b53492e0d35bf7
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-08-26 02:22:36 -0400

    Update 7.9.9.m

--- c7/7.9.9.m
+++ c7/7.9.9.m
@@ -1,4 +1,8 @@
 %7.9.9 from LJSA
+%Expected duration until the nutrient is depleted N(T)=0 for enzyme kinetics model
+%added as function of the method from book with default values
+%N(0)=301
+%
 function retval = nutrientDepletion(nsim=10000, einf=120, pinf=301, k1=1.66*10^(-3), k_1=10^(-4), k2=0.1)
     kmax=(k2*einf);
     km=(k_1+k2)/k1;
