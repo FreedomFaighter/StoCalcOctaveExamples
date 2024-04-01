1fad54ec62d91fa42bae4dfc9624f51cd57b4ab4
John H. Freeman <john@freemania.pro>
2024-04-01 16:46:03 -0500

    chapter 2 problems functionalized with default parameters

--- chapter2/Monte Carlo 2.25.m
+++ chapter2/Monte Carlo 2.25.m
@@ -1,10 +1,11 @@
-m=10^6; p=0.45;
-x=zeros(m,1);
-for i=1:100
-    r=rand(m,1);
-    y1=(r<p);
-    x=x-y1;
-    y2=(r>=p);
-    x=x+y2;
+function [ans] = monteCarlo(m=10^6, p=0.45, n=100)
+    x=zeros(m,1);
+    for i=1:n
+        r=rand(m,1);
+        y1=(r<p);
+        x=x-y1;
+        y2=(r>=p);
+        x=x+y2;
+    end
+    ans=mean(x)
 end
-ans=mean(x)
